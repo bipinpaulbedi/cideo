f861e11f4f2d7b39a2aa55dea4e7a0440d1198ac
Bipin Paul Bedi <bipinpaulbedi@MacBook-Pro.local>
2020-06-05 12:51:19 +1000

    Completed first beta version

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Replay10 from '@material-ui/icons/Replay10';
 import AddCircle from '@material-ui/icons/AddCircle';
 import RemoveCircle from '@material-ui/icons/RemoveCircle';
 import Forward10 from '@material-ui/icons/Forward10';
+import ClearAll from '@material-ui/icons/ClearAll';
 
 function App() {
   let [playBackRate, setPlayBackRate] = useState(1.0)
@@ -20,13 +21,44 @@ function App() {
     }
   }, [playBackRate, moveTimeLine]);
 
+  useEffect(() => {
+    const handleKeyPress = (e: any) => {
+        switch(e.keyCode){
+          case 83:
+          case 115:
+            setPlayBackRate(p => p - 0.1 > 0.1 ? p - 0.1 : 0.1);
+            break;
+          case 68:
+          case 100:
+            setPlayBackRate(p => p + 0.1);
+            break;
+          case 82:
+          case 114:
+            setPlayBackRate(1);
+            break;
+          case 90:
+          case 112:
+            setmoveTimeLine(-10);
+            break;
+          case 88:
+          case 120:
+            setmoveTimeLine(10);
+        } 
+    };
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+        window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
   return (
     <div className="CideoController" ref={refCiderController}>
-      <IconButton onClick={() => setmoveTimeLine(-10)}><Replay10 /></IconButton>
-      <IconButton onClick={() => setPlayBackRate(p => p + 0.1)}><AddCircle /></IconButton>      
-      <IconButton onClick={() => setPlayBackRate(p => p - 0.1 > 0.1 ? p - 0.1 : 0.1)}><RemoveCircle /></IconButton>
-      <IconButton onClick={() => setmoveTimeLine(10)}><Forward10 /></IconButton>
-      <span>{playBackRate.toFixed(1)}x</span>
+      <IconButton onClick={(e) => {e.preventDefault(); setmoveTimeLine(-10)}}><Replay10 /></IconButton>
+      <IconButton onClick={(e) => {e.preventDefault();setPlayBackRate(p => p + 0.1)}}><AddCircle /></IconButton>      
+      <IconButton onClick={(e) => {e.preventDefault();setPlayBackRate(1)}}><ClearAll /></IconButton>      
+      <IconButton onClick={(e) => {e.preventDefault();setPlayBackRate(p => p - 0.1 > 0.1 ? p - 0.1 : 0.1)}}><RemoveCircle /></IconButton>
+      <IconButton onClick={(e) => {e.preventDefault();setmoveTimeLine(10)}}><Forward10 /></IconButton>
     </div>
   );
 }
